152d036752e96fd3b00e246028243fb95bb51a81
btatarashvili@aldagi.ge <btatarashvili@BTATARASHVILI>
2025-12-24 15:02:21 +0400

    Add project files.

---
+++ ReflectionTest/Extenstions/ObjectExtenstions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTest.Extenstions
+{
+    public static class ObjectExtenstions
+    {
+        public static void PrintObjectStructure(this object o, string indentation = "")
+        {
+            if (o == null)
+            {
+                Console.WriteLine("null");
+                return;
+            }
+
+            var type = o.GetType();
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            Console.WriteLine($"{indentation}Object of Class \"{type.FullName}\"");
+            Console.WriteLine($"{indentation}--------------------------------");
+
+            var propIndent = indentation + "    ";
+
+            foreach (var property in properties)
+            {
+                // skip indexers
+                if (property.GetIndexParameters().Length > 0)
+                    continue;
+
+                var value = property.GetValue(o);
+
+                if (IsSimpleType(property.PropertyType))
+                {
+                    Console.WriteLine($"{propIndent}{property.Name}=\"{value}\"");
+                }
+                else
+                {
+                    if (value is null)
+                    {
+                        Console.WriteLine($"{propIndent}{property.Name}=null");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{propIndent}{property.Name}:");
+                        PrintObjectStructure(value, propIndent + "    ");
+                    }
+                }
+            }
+        }
+
+        private static bool IsSimpleType(Type type)
+        {
+            type = Nullable.GetUnderlyingType(type) ?? type;
+
+            return type.IsPrimitive
+                || type.IsEnum
+                || type == typeof(string)
+                || type == typeof(decimal)
+                || type == typeof(DateTime)
+                || type == typeof(DateTimeOffset)
+                || type == typeof(Guid)
+                || type == typeof(TimeSpan);
+        }
+    }
+}
---
+++ ReflectionTest/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using ReflectionTest.Extenstions;
+
+Name n = new Name();
+n.FirstName = "John";
+n.LastName = "Doe";
+Person p = new Person();
+p.Age = 55;
+p.BankBalance = 102.2;
+p.Identification = new Identification()
+{
+    IdentificationNumber = "11111111111",
+    Name = n
+};
+p.Vehicle = new Vehicle() { Brand = "BMW", Model = "X5" };
+
+p.PrintObjectStructure();
+Console.ReadKey();
+
+public class Name
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Person
+{
+    public int Age { get; set; }
+    public double BankBalance { get; set; }
+    public Identification Identification { get; set; }
+    public Vehicle Vehicle { get; set; }
+}
+
+public class Vehicle
+{
+    public string Model { get; set; }
+    public string Brand { get; set; }
+}
+
+public class Identification
+{
+    public string IdentificationNumber { get; set; }
+    public Name Name { get; set; }
+}
